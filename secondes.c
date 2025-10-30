c1e6130b13168f3d6453d41d8e8315e96e1bb682
LOtux_VO <edgar.pulles@gmail.com>
2025-10-30 12:53:43 +0100

    secondes changes

--- secondes.c
+++ secondes.c
@@ -6,11 +6,13 @@ int main() {
     printf("Entrez un nombre de secondes : ");
     scanf("%d", &secondes);
 
+    int annees = secondes / (3600 * 24 * 365);
+    int jours = secondes / (3600 * 24);
     int heures = secondes / 3600;
     int minutes = (secondes % 3600) / 60;
     int sec = secondes % 60;
 
-    printf("%d secondes correspondent a %d heures, %d minutes et %d secondes.\n", secondes, heures, minutes, sec);
+    printf("%d secondes correspondent a %d annees, %d jours, %d heures, %d minutes et %d secondes.\n",secondes, annees, jours, heures, minutes, sec);
 
     return 0;
 }
