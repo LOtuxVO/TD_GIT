35f680e6ae7d5052946c2b14935a97baa532e62b
LOtux_VO <edgar.pulles@gmail.com>
2025-11-17 13:46:49 +0100

    dessin pointe etoile

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -3,11 +3,6 @@
 int main() {
     int taille;
 
-    int width = taille * taille;
-    int max_height = 3 * taille - 1;
-
-    char etoile[31][123];
-
     printf("Entrez une taille (entre 5 et 10) : ");
     scanf("%d", &taille);
 
@@ -16,20 +11,49 @@ int main() {
         scanf("%d", &taille);
     }
 
+    char etoile[31][123];
+    int width = taille * taille;
+    int max_height = 3 * taille - 1;
+
     for (int i = 0; i < max_height; i++) {
         for (int j = 0; j < width; j++) {
             etoile[i][j] = ' ';
         }
     }
 
-    printf("\nEtoile de taille %d x %d.\n", taille, taille);
+    int midle = width / 2;
+
+    int gauche = midle;
+    int droite = midle;
+
+    for (int i = 0; i < taille; i++) {
+
+        for (int j = 0; j < width; j++) {
+
+            if (j == gauche || j == droite) {
+                etoile[i][j] = 'A';
+            }
+            else if (j > gauche && j < droite) {
+                etoile[i][j] = 'S';
+            }
+        }
+
+        gauche--;
+        droite++;
+    }
+
+    for (int k = gauche + 1; k < droite; k++) {
+        etoile[taille][k] = '_';
+    }
+
+        printf("\nEtoile de taille %d x %d.\n", taille, taille);
     for (int i = 0; i < max_height; i++) {
         for (int j = 0; j < width; j++) {
-            printf("%c",etoile[i][j]);
+            printf("%c", etoile[i][j]);
         }
         printf("\n");
     }
 
+
     return 0;
 }
-
