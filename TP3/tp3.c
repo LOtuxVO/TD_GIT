8445e765d473388147cd215654a4abeb3aab98ca
LOtux_VO <edgar.pulles@gmail.com>
2025-11-17 13:10:59 +0100

    demande a l'infini tant que valeur pas bonne

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -9,7 +9,7 @@ int main() {
     printf("Entrez une taille (entre 5 et 10) : ");
     scanf("%d", &taille);
 
-    if (taille < 5 || taille > 10) {
+    while(taille < 5 || taille > 10) {
         printf("Erreur : la taille doit être entre 5 et 10.\n");
         printf("Entrez une taille valide : ");
         scanf("%d", &taille);
