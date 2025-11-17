56a90119eb72b13e049ca99ad294469f6fa3b80f
LOtux_VO <edgar.pulles@gmail.com>
2025-11-17 15:29:11 +0100

    dessin etoile finale qui ressemble a un sapin, une fusee ou un avion de chasse mais pas beaucoup a une etoile

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -1,160 +1,99 @@
 #include <stdio.h>
 
-int main() {
-    int taille = 0;
+#define MAX_LARGEUR 80
+#define MAX_HAUTEUR 80
 
-    printf("Entrez une taille (recommandee 5 a 10) : ");
-    
+int main() {
+    int taille;
+
+    printf("Entrez une taille (5 à 10) : ");
     while (scanf("%d", &taille) != 1 || taille < 5 || taille > 10) {
-        if (scanf("%d", &taille) != 1) {
-            int c;
-            while ((c = getchar()) != '\n' && c != EOF);
-        }
-        printf("Erreur : la taille doit etre un nombre entier entre 5 et 10. Reessayez : ");
+        printf("Erreur. Reessayez : ");
+        int c; while ((c = getchar()) != '\n' && c != EOF);
     }
-    
-    const int MAX_LARGEUR = 45; 
-    const int MAX_HAUTEUR = 32;
 
     char etoile[MAX_HAUTEUR][MAX_LARGEUR];
-    
-    int largeur_utilisee = 4 * taille + 5;
-    int hauteur_utilisee = 3 * taille + 2;
 
-
-    for (int i = 0; i < MAX_HAUTEUR; i++) {
-        for (int j = 0; j < MAX_LARGEUR; j++) {
+    for (int i = 0; i < MAX_HAUTEUR; i++)
+        for (int j = 0; j < MAX_LARGEUR; j++)
             etoile[i][j] = ' ';
-        }
-    }
 
     int mid = MAX_LARGEUR / 2;
 
     for (int i = 0; i < taille; i++) {
-        int left = mid - i;
+        int left  = mid - i;
         int right = mid + i;
-        
-        etoile[i][left] = 'A';
+        etoile[i][left]  = 'A';
         etoile[i][right] = 'A';
-        
-        for (int j = left + 1; j < right; j++) {
+        for (int j = left + 1; j < right; j++)
             etoile[i][j] = 'S';
-        }
-    }
-    etoile[0][mid] = 'A';
-
-    int ligne_separation = taille;
-    int base_pointe_left = mid - (taille - 1);
-    int base_pointe_right = mid + (taille - 1);
-
-    for (int k = base_pointe_left; k <= base_pointe_right; k++) {
-        etoile[ligne_separation][k] = '_';
     }
 
-    int ligne_corps_debut = ligne_separation + 1;
+    int ligne_corps_debut = taille;
+    int largeur_corps = taille + 2;
+
     int hauteur_corps = taille;
-    int left_corps = base_pointe_left - 1;
-    int right_corps = base_pointe_right + 1;
-
     for (int i = 0; i < hauteur_corps; i++) {
-        int ligne = ligne_corps_debut + i;
-        
-        if (i == 0) {
-            etoile[ligne][left_corps] = '"';
-            etoile[ligne][right_corps] = '"';
-            for (int j = left_corps + 1; j < right_corps; j++) {
-                etoile[ligne][j] = 'S';
-            }
-        } else if (i == 1 || i == 2) {
-            etoile[ligne][left_corps] = '\'';
-            etoile[ligne][right_corps] = '\'';
-            
-            int inner_left = left_corps + 1 + (i - 1);
-            int inner_right = right_corps - 1 - (i - 1);
-            
-            etoile[ligne][inner_left] = 'V';
-            etoile[ligne][inner_right] = 'V';
-            
-            for (int j = inner_left + 1; j < inner_right; j++) {
-                etoile[ligne][j] = 'S';
-            }
-        } else if (i >= 3) {
-            int current_left = mid - (taille - 1 - (i - 3));
-            int current_right = mid + (taille - 1 - (i - 3));
-            
-            etoile[ligne][current_left] = '.';
-            etoile[ligne][current_right] = '.';
-            
-            for (int j = current_left + 1; j < current_right; j++) {
-                etoile[ligne][j] = 'S';
-            }
-        }
+        int y = ligne_corps_debut + i;
+        int left  = mid - largeur_corps + i / 2;
+        int right = mid + largeur_corps - i / 2;
+        etoile[y][left]  = (i < 2 ? '"' : '.');
+        etoile[y][right] = (i < 2 ? '"' : '.');
+        for (int j = left + 1; j < right; j++)
+            etoile[y][j] = 'S';
     }
 
-    int ligne_jambes_debut = ligne_corps_debut + hauteur_corps;
+    int ligne_bras_haut = ligne_corps_debut;
+    int hauteur_bras = taille;
+    for (int i = 0; i < hauteur_bras; i++) {
+        int y = ligne_bras_haut + i;
+
+        int xg1 = mid - 1 - i*3;
+        int xg2 = mid - 1;
+        etoile[y][xg1] = '.';
+        etoile[y][xg2] = '"';
+        for (int j = xg1 + 1; j < xg2; j++)
+            etoile[y-1 >= 0 ? y-1 : y][j] = 'S';
+
+        int xd1 = mid + 1;
+        int xd2 = mid + 1 + i*3;
+        etoile[y][xd1] = '"';
+        etoile[y][xd2] = '.';
+        for (int j = xd1 + 1; j < xd2; j++)
+            etoile[y-1 >= 0 ? y-1 : y][j] = 'S';
+    }
+
+
+
+
+    int ligne_jambes = ligne_corps_debut + hauteur_corps - 1;
     int hauteur_jambes = taille;
-    int debut_centre_gauche = mid - 1;
-    int debut_centre_droit = mid + 1;
-
     for (int i = 0; i < hauteur_jambes; i++) {
-        int ligne = ligne_jambes_debut + i;
+        int y = ligne_jambes + i;
 
-        int left_leg_g = debut_centre_gauche - i;
-        int right_leg_g = debut_centre_gauche;
-        if (left_leg_g >= 0) {
-            etoile[ligne][left_leg_g] = '.';
-            etoile[ligne][right_leg_g + 1] = '"';
-            for (int j = left_leg_g + 1; j <= right_leg_g; j++) {
-                etoile[ligne][j] = 'S';
-            }
-        }
+        int lg = mid - 1 - i;
+        int rg = mid - 1;
+        etoile[y][lg] = '.';
+        etoile[y][rg] = '"';
+        for (int j = lg + 1; j < rg; j++)
+            etoile[y][j] = 'S';
 
-        int left_leg_d = debut_centre_droit;
-        int right_leg_d = debut_centre_droit + i;
-        if (right_leg_d < MAX_LARGEUR) {
-            etoile[ligne][left_leg_d - 1] = '"';
-            etoile[ligne][right_leg_d] = '.';
-            for (int j = left_leg_d; j < right_leg_d; j++) {
-                etoile[ligne][j] = 'S';
-            }
-        }
-
-        if (i < 2) {
-             etoile[ligne][mid - 1] = ' ';
-             etoile[ligne][mid] = ' ';
-             etoile[ligne][mid + 1] = ' ';
-        } else {
-             etoile[ligne][mid] = ' ';
-        }
+        int ld = mid + 1;
+        int rd = mid + 1 + i;
+        etoile[y][ld] = '"';
+        etoile[y][rd] = '.';
+        for (int j = ld + 1; j < rd; j++)
+            etoile[y][j] = 'S';
     }
 
-    printf("\nEtoile finale (Taille %d) :\n", taille);
-
-    int hauteur_finale = 0;
-    for (int i = 0; i < hauteur_utilisee; i++) {
-        int ligne_vide = 1;
-        for (int j = 0; j < largeur_utilisee; j++) { 
-            if (etoile[i][j] != ' ') {
-                ligne_vide = 0;
-                break;
-            }
-        }
-        if (!ligne_vide) {
-            hauteur_finale = i + 1;
-        }
-    }
-
-    for (int i = 0; i < hauteur_finale; i++) {
-        int max_j = 0;
-        for (int j = 0; j < largeur_utilisee; j++) {
-            if (etoile[i][j] != ' ') {
-                max_j = j;
-            }
-        }
-        for (int j = 0; j <= max_j; j++) {
-            printf("%c", etoile[i][j]);
-        }
-        printf("\n");
+    int h_max = ligne_jambes + hauteur_jambes;
+    for (int i = 0; i <= h_max; i++) {
+        int last = 0;
+        for (int j = 0; j < MAX_LARGEUR; j++)
+            if (etoile[i][j] != ' ') last = j;
+        for (int j = 0; j <= last; j++)
+            putchar(etoile[i][j]);
+        putchar('\n');
     }
 
     return 0;
