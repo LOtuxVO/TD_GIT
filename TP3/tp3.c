1db80501f8fc50cefa39fd0b36cb95525358d0c7
LOtux_VO <edgar.pulles@gmail.com>
2025-11-17 13:07:39 +0100

    variable taille v2

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -6,12 +6,7 @@ int main() {
     printf("Entrez une taille (entre 5 et 10) : ");
     scanf("%d", &taille);
 
-    if (taille < 5) {
-        printf("Erreur : la taille doit être entre 5 et 10.\n");
-        printf("Entrez une taille valide : ");
-        scanf("%d", &taille);
-    }
-    else if (taille > 10) {
+    if (taille < 5 || taille > 10) {
         printf("Erreur : la taille doit être entre 5 et 10.\n");
         printf("Entrez une taille valide : ");
         scanf("%d", &taille);
