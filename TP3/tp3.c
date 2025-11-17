ca9fa94416499c1f678135431b2fd1d898af8f16
LOtux_VO <edgar.pulles@gmail.com>
2025-11-17 13:34:23 +0100

    tableau avec espace

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -6,19 +6,30 @@ int main() {
     int width = taille * taille;
     int max_height = 3 * taille - 1;
 
-    char etoile[123][31];
+    char etoile[31][123];
 
     printf("Entrez une taille (entre 5 et 10) : ");
     scanf("%d", &taille);
 
     while(taille < 5 || taille > 10) {
-        printf("Erreur : la taille doit être entre 5 et 10.\n");
-        printf("Entrez une taille valide : ");
+        printf("Erreur : la taille doit etre entre 5 et 10 :\n");
         scanf("%d", &taille);
     }
-    
+
+    for (int i = 0; i < max_height; i++) {
+        for (int j = 0; j < width; j++) {
+            etoile[i][j] = ' ';
+        }
+    }
+
+    printf("\nEtoile de taille %d x %d.\n", taille, taille);
+    for (int i = 0; i < max_height; i++) {
+        for (int j = 0; j < width; j++) {
+            printf("%c",etoile[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
-
 
