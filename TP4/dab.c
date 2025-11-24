93f6edfa88ac856c96dd929139c31f977eef6a27
LOtux_VO <edgar.pulles@gmail.com>
2025-11-24 14:42:28 +0100

    calcul et affichage du nombre de billet et de quel billet de 200, 100, 50, 20, 10, 5

--- TP4/dab.c
+++ TP4/dab.c
@@ -8,8 +8,9 @@ void afficher_menu() {
 }
 
 int saisir_montant() {
-    int montant;
-    do {
+    int montant = 0;
+
+    while (montant <= 0 || montant > 1000 || montant % 5 != 0) {
         printf("\nEntrez le montant a retirer (max 1000 EUR, multiple de 5) : ");
         if (scanf("%d", &montant) != 1) {
             printf("Erreur : Saisie invalide. Veuillez entrer un nombre.\n");
@@ -28,9 +29,35 @@ int saisir_montant() {
             printf("Erreur : Le montant doit etre un multiple de 5.\n");
         }
 
-    } while (montant <= 0 || montant > 1000 || montant % 5 != 0);
+    }
 
     return montant;
+}
+
+
+void calcul_distribution(int montant) {
+    int nb200 = 0, nb100 = 0, nb50 = 0, nb20 = 0, nb10 = 0, nb5 = 0;
+
+    printf("\nDistribution de votre retrait de %d EUR :\n", montant);
+
+    nb200 = montant / 200;
+    montant %= 200;
+
+    nb100 = montant / 100;
+    montant %= 100;
+
+    nb50 = montant / 50;
+    montant %= 50;
+
+    nb20 = montant / 20;
+    montant %= 20;
+
+    nb10 = montant / 10;
+    montant %= 10;
+
+    nb5 = montant / 5;
+
+    printf("- %d billet(s) de 200 EUR\n- %d billet(s) de 100 EUR\n- %d billet(s) de 50 EUR\n- %d billet(s) de 20 EUR\n- %d billet(s) de 10 EUR\n- %d billet(s) de 5 EUR\n",nb200, nb100, nb50, nb20, nb10, nb5);
 }
 
 int main() {
@@ -51,6 +78,8 @@ int main() {
                 printf("Vous avez choisis de retirer de l'argent\n");
                 int montant_retrait = saisir_montant();
                 printf("\nDemande de retrait de %d EUR\n", montant_retrait);
+                calcul_distribution(montant_retrait);
+                Sleep(5000);
                 break;
 
             case 0:
