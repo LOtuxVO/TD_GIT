853a998a1ac3e5490f619481da6aaa7817e10038
LOtux_VO <edgar.pulles@gmail.com>
2025-11-24 14:33:15 +0100

    demande du montant et son affichage et logique d'erreur si superieur a 1000 ou inferieur a 5 ou pas multiple de 5

--- TP4/dab.c
+++ TP4/dab.c
@@ -7,6 +7,32 @@ void afficher_menu() {
     printf("0 - Quitter\n");
 }
 
+int saisir_montant() {
+    int montant;
+    do {
+        printf("\nEntrez le montant a retirer (max 1000 EUR, multiple de 5) : ");
+        if (scanf("%d", &montant) != 1) {
+            printf("Erreur : Saisie invalide. Veuillez entrer un nombre.\n");
+            while (getchar() != '\n');
+            montant = -1;
+            continue;
+        }
+
+        if (montant <= 0) {
+            printf("Erreur : Le montant doit etre positif.\n");
+        } else if (montant > 1000) {
+            printf("Erreur : Le montant ne peut pas depasser 1000 EUR.\n");
+        }else if (montant < 5){
+            printf("Erreur : Le montant ne peut pas etre inferieur a 5 EUR.\n");
+        }else if (montant % 5 != 0) {
+            printf("Erreur : Le montant doit etre un multiple de 5.\n");
+        }
+
+    } while (montant <= 0 || montant > 1000 || montant % 5 != 0);
+
+    return montant;
+}
+
 int main() {
     int choix = -1;
 
@@ -21,11 +47,16 @@ int main() {
 
         switch (choix) {
             case 1:
-                printf("\nVous avez choisi de retirer de l'argent.\n");
+                
+                printf("Vous avez choisis de retirer de l'argent\n");
+                int montant_retrait = saisir_montant();
+                printf("\nDemande de retrait de %d EUR\n", montant_retrait);
                 break;
+
             case 0:
                 printf("\nAu revoir !\n");
                 break;
+
             default:
                 printf("\nChoix invalide. Veuillez reessayer.\n");
                 break;
