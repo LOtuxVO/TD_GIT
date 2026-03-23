bb47ae6d71e6034216ecb77cdcc41dd3f688fe2c
LOtux_VO <edgar.pulles@gmail.com>
2026-03-23 14:03:39 +0100

    modification de la fonction estPair pour que tous les tests soient bon

--- S2_TP4_TU/S2_TP4_TU_test/S2_TP4_TU_test.cpp
+++ S2_TP4_TU/S2_TP4_TU_test/S2_TP4_TU_test.cpp
@@ -12,15 +12,27 @@ namespace S2TP4TUtest
 	TEST_CLASS(S2TP4TUtest)
 	{
 	public:
-		
+
 		TEST_METHOD(TestEstPair)
 		{
-			Assert::AreEqual(estPair(6), 0);
+			Assert::AreEqual(estPair(4), 1);
 		}
 
 		TEST_METHOD(TestEstPair2)
 		{
-			Assert::AreEqual(estPair(7), 1);
+			Assert::AreEqual(estPair(7), 0);
+		}
+		TEST_METHOD(TestEstPair3)
+		{
+			Assert::AreEqual(estPair(0), 1);
+		}
+		TEST_METHOD(TestEstPair4)
+		{
+			Assert::AreEqual(estPair(-2), 1);
+		}
+		TEST_METHOD(TestEstPair5)
+		{
+			Assert::AreEqual(estPair(-3), 0);
 		}
 	};
 }
